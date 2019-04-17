208d4ad1eac7ec29d4bfdc0fd6a9b217b14b2d00
mind-v <mind.vodniki@gmail.com>
2019-04-17 18:29:56 +0500

    Update arping.php
    added form(ip) validation

--- arping.php
+++ arping.php
@@ -11,19 +11,25 @@
 
 <div class="container">
 <form action="/arping.php" method="get">
-        IP <input type="text" name="ip">
-        <input type="submit"><br><br>
+        IP <input type="text" name="ip"><input type="submit">
+        <br><br>
         Enter IP<br>
         Press Submit<br>
         Wait 5 seconds, you'll get a reply.
 </form>
 
-<?php
+<?php   
 set_time_limit(30);
 
 $ip = $_GET['ip'];
-$output = shell_exec("/usr/sbin/arping -c5 '{$ip}'");
 
-echo "<pre >$output</pre>";
-?>
+if (filter_var($ip, FILTER_VALIDATE_IP)) {
+        $output = shell_exec("/usr/sbin/arping -c5 '{$ip}'");
+        echo "<pre>$output</pre>";
+}
+
+else {  
+        echo "<pre>Please supply a valid IPv4 or IPv6 address</pre>";
+}
+?>      
 </div>
